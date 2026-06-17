6f4ab49c0c0d1b818d379d624754bf1cff007e58
Vasia <veselvv12@yandex.ru>
2026-06-17 19:08:24 +0300

    Update: replace structures from app.c to app.h

--- src/app.c
+++ src/app.c
@@ -5,54 +5,9 @@
 #include "audio.h"
 #include "dirwork.h"
 #include "music_duration.h"
+#include "app.h"
 
 
-typedef enum{
-    PAUSED,
-    PLAYED,
-    DISABLED,
-    ON,
-    OFF
-}ButtonState;
-
-
-
-
-typedef struct{
-    Mix_Music *music;
-    double duration;
-    char filename[512];
-    char folderpath[512];
-    int current_track_index;
-    int last_track_index;
-}MusikData;
-
-
-typedef struct{
-    GtkWindow *main_window;
-    GtkWidget *main_play_button;
-    GtkWidget *main_play_button_icon;
-    GtkWidget *main_prev_button;
-    GtkWidget *main_scale;
-    GtkWidget *main_label_time;
-    GtkWidget *main_tracks_listbox;
-    GtkWidget *main_button_update;
-    GtkWidget *main_button_open_folder;
-    GtkWidget *main_label_name;
-    GtkWidget *main_next_button;
-    GtkWidget *main_volume_scale;
-    GtkWidget *main_button_mode;
-    GtkWidget *main_button_one_track;
-    GtkWidget *main_button_one_track_loop;
-    ButtonState current_button_play_state;
-    ButtonState current_button_mod_state;
-    ButtonState current_main_button_one_track_loop;
-    ButtonState current_main_button_one_track_state;
-    MusikData *mData;
-    gboolean is_seeking;
-    guint timeout_id;
-    gboolean is_dragging;
-}AppData;
 
 
 
